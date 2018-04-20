6c636b99a65ce29dcd5bf7037fedc8d8ef282956
Matthew Swarbrick <matthew.swarbrick@rsmuk.com>
2018-04-20 16:27:49 +0100

    add basic documentation for step 1 & 2 of the kata

--- GildedRose.Aurelia/src/app.ts
+++ GildedRose.Aurelia/src/app.ts
@@ -9,7 +9,9 @@ export class App {
     config.title = "Gilded Rose Kata"
     config.map([
       { route: '', moduleId: PLATFORM.moduleName("./home/home"), name: 'home', title: 'Home' },
-      { route: 'step1', moduleId: PLATFORM.moduleName("./instructions/step1"), name: 'step1', title: 'Step 1' }
+      { route: 'step1', moduleId: PLATFORM.moduleName("./instructions/step1"), name: 'step1', title: 'Step 1' },
+      { route: 'step2', moduleId: PLATFORM.moduleName("./instructions/step2"), name: 'step2', title: 'Step 2' },
+      { route: 'step3', moduleId: PLATFORM.moduleName("./instructions/step3"), name: 'step3', title: 'Step 3' }
     ]);
 
     config.mapUnknownRoutes({ route: '', moduleId: PLATFORM.moduleName("./home/home"), name:'home', title: 'Home' });
